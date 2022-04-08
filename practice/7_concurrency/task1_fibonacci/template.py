1b4449bb626fdacfac2f4cda586cbfe2515b6521
Yevhen Prokhoda <e.prohoda@gmail.com>
2022-04-08 23:31:58 +0300

    Part7 done

--- practice/7_concurrency/task1_fibonacci/template.py
+++ practice/7_concurrency/task1_fibonacci/template.py
@@ -1,6 +1,6 @@
 import os
+from multiprocessing import Process
 from random import randint
-
 
 OUTPUT_DIR = './output'
 RESULT_FILE = './output/result.csv'
@@ -10,22 +10,38 @@ def fib(n: int):
     """Calculate a value in the Fibonacci sequence by ordinal number"""
 
     f0, f1 = 0, 1
-    for _ in range(n-1):
+    for _ in range(n - 1):
         f0, f1 = f1, f0 + f1
     return f1
 
 
 def func1(array: list):
-    pass
+    for i in array:
+        with open(str(i) + ".txt", 'w') as f:
+            f.write(str(fib(i)))
 
 
 def func2(result_file: str):
-    pass
+    with open(result_file, 'a') as result_file:
+        for file in os.listdir():
+            if file.endswith(".txt") and file != result_file.name:
+                with open(file, "r") as f:
+                    data = str(file.replace(".txt", "") + ", " + f.read() + "\n")
+                    result_file.write(data)
 
 
 if __name__ == '__main__':
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    func1(array=[randint(1000, 100000) for _ in range(1000)])
-    func2(result_file=RESULT_FILE)
+    # run ordinary
+    # func1(array=[randint(1, 5) for _ in range(5)])
+    # func2(result_file="result.txt")
+
+    # run in parrallel
+    p1 = Process(target=func1, args=([randint(1, 5) for _ in range(5)], ))
+    p2 = Process(target=func2, args=("result.txt",))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,23 +1,49 @@
-API_KEY = "YOUR_KEY"
+import json
+import multiprocessing
+from datetime import timedelta, datetime
+from pathlib import Path
+from multiprocessing import Pool
+import requests as requests
+
+API_KEY = "LnRSSryolbcE54eb4gXoaYxgeJAvzl9Pp49TyQQa"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
 OUTPUT_IMAGES = './output'
 
 
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    delta = end_date - start_date
+    urls = []
+    for i in range(delta.days + 1):
+        day = start_date + timedelta(days=i)
+        day_str = str(day).split(" ")[0]
+
+        response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={day_str}")
+
+        url = json.loads(response.text)['url']
+        urls.append(url)
+
+    return urls
 
 
-def download_apod_images(metadata: list):
-    pass
+def download_apod_image(url):
+    name = url.split('/')[-1]
+    img_data = requests.get(url).content
+    path = Path("Image_folder", name)
+    with open(path, 'wb') as handler:
+        handler.write(img_data)
 
 
 def main():
-    metadata = get_apod_metadata(
+    urls_list = get_apod_metadata(
         start_date='2021-08-01',
-        end_date='2021-09-30',
+        end_date='2021-08-10',
         api_key=API_KEY,
     )
-    download_apod_images(metadata=metadata)
+
+    pool = Pool()
+    pool.map(download_apod_image, urls_list)
 
 
 if __name__ == '__main__':
